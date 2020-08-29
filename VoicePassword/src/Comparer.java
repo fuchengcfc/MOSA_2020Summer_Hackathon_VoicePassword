45d44a343602b16f3b16bc26aadcdd2ae7492eb1
fuchengcfc <fcui@seas.upenn.edu>
2020-08-30 01:33:58 +1000

    add test and change comparer class

--- VoicePassword/src/Comparer.java
+++ VoicePassword/src/Comparer.java
@@ -1,46 +1,46 @@
-import java.io.ByteArrayOutputStream;
-import java.io.File;
 
-import javax.sound.sampled.AudioInputStream;
-import javax.sound.sampled.AudioSystem;
 
 import com.musicg.fingerprint.FingerprintSimilarity;
-import com.musicg.fingerprint.FingerprintSimilarityComputer;
+import com.musicg.wave.Wave;
 
 public class Comparer {
 	
-	public static byte[] readAudioFileData(final String filePath) {
-	    byte[] data = null;
-	    try {
-	        final ByteArrayOutputStream baout = new ByteArrayOutputStream();
-	        final File file = new File(filePath);
-	        final AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(file);
-
-	        byte[] buffer = new byte[4096];
-	        int c;
-	        while ((c = audioInputStream.read(buffer, 0, buffer.length)) != -1) {
-	            baout.write(buffer, 0, c);
-	        }
-	        audioInputStream.close();
-	        baout.close();
-	        data = baout.toByteArray();
-	    } catch (Exception e) {
-	        e.printStackTrace();
-	    }
-	    return data;
-	}
+//	public static byte[] readAudioFileData(final String filePath) {
+//	    byte[] data = null;
+//	    try {
+//	        final ByteArrayOutputStream baout = new ByteArrayOutputStream();
+//	        final File file = new File(filePath);
+//	        final AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(file);
+//	        
+//	        System.out.println(audioInputStream.getFrameLength());
+//
+//	        byte[] buffer = new byte[4096];
+//	        int c;
+//	        while ((c = audioInputStream.read(buffer, 0, buffer.length)) != -1) {
+//	            baout.write(buffer, 0, c);
+//	        }
+//	        audioInputStream.close();
+//	        baout.close();
+//	        data = baout.toByteArray();
+//	    } catch (Exception e) {
+//	        e.printStackTrace();
+//	    }
+//	    return data;
+//	}
 	
 	
 	public static boolean compareTwoFile(String fileName1, String fileName2 ) {
 		
-		byte[] firstAudio = readAudioFileData(fileName1);
-		byte[] secondAudio = readAudioFileData(fileName2);
+		
+		Wave w1 = new Wave(fileName1);
+		Wave w2 = new Wave(fileName2);
+		
 
-		FingerprintSimilarityComputer fingerprint = 
-		            new FingerprintSimilarityComputer(firstAudio, secondAudio);
+		FingerprintSimilarity fingerprint = w1.getFingerprintSimilarity(w2);
 
-		FingerprintSimilarity fingerprintSimilarity = fingerprint.getFingerprintsSimilarity();
-		float score = fingerprintSimilarity.getScore();
+		float score = fingerprint.getScore();
+		
+		System.out.println(score);
 		
 		if (score > 0.8) {
 			return true;
---
+++ VoicePassword/src/test.java
@@ -0,0 +1,13 @@
+
+public class test {
+	
+	public static void main(String[] args) {
+		boolean checkTwoFiles = Comparer.compareTwoFile("1.wav", "1.wav");
+		
+		System.out.println(checkTwoFiles);
+		
+		
+		
+	}
+
+}
