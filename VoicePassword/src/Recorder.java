4cb7831d41fc6f1fd77e5aca990ad838145b2cab
fuchengcfc <fcui@seas.upenn.edu>
2020-08-30 02:45:38 +1000

    change recorder class and test class

--- VoicePassword/src/Recorder.java
+++ VoicePassword/src/Recorder.java
@@ -1,5 +1,66 @@
+import java.io.File;
+import java.io.IOException;
+
+import javax.sound.sampled.AudioFileFormat;
+import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.DataLine;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.TargetDataLine;
 
 public class Recorder {
+	
+	
+	public static void record(String filename) {
+		
+		try {
+		AudioFormat format = new AudioFormat(AudioFormat.Encoding.PCM_SIGNED, 44100, 16, 2, 4, 44100, false);
+		DataLine.Info info = new DataLine.Info(TargetDataLine.class, format);
+		
+		final TargetDataLine targetLine = (TargetDataLine)AudioSystem.getLine(info);
+		targetLine.open();
+		targetLine.start();
+		
+		Thread thread = new Thread()
+		{
+			@Override public void run()
+			{
+				AudioInputStream audioStream = new AudioInputStream(targetLine);
+				File audioFile = new File(filename);
+				try {
+					AudioSystem.write(audioStream, AudioFileFormat.Type.WAVE, audioFile);
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+				
+				
+			}
+		};
+		
+		thread.start();
+		Thread.sleep(3000);
+		targetLine.stop();
+		targetLine.close();
+		
+			
+			
+			
+		} catch (LineUnavailableException e) {
+			e.printStackTrace();
+
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+
+		}
+		
+		
+		
+		
+		
+		
+		
+	}
 	
 	
 
--- VoicePassword/src/test.java
+++ VoicePassword/src/test.java
@@ -1,8 +1,21 @@
+import java.util.Scanner;
 
 public class test {
 	
 	public static void main(String[] args) {
-		boolean checkTwoFiles = Comparer.compareTwoFile("1.wav", "1.wav");
+		
+		System.out.println("password record begin");
+		Recorder.record("password.wav");
+		System.out.println("password record finish, press 1 to continue");
+		Scanner scanner = new Scanner(System.in);
+		int num = scanner.nextInt();
+		if (num == 1) {
+			System.out.println("voice to solve begin");
+			Recorder.record("try.wav");
+			System.out.println("voice to solve finish");
+		}
+		
+		boolean checkTwoFiles = Comparer.compareTwoFile("password.wav", "try.wav");
 		
 		System.out.println(checkTwoFiles);
 		
