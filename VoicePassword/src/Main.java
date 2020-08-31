560e049cbd5b7dd8c50781c85bbbf1794d5ecea2
junojiahui <51941324+junojiahui@users.noreply.github.com>
2020-08-30 17:50:31 -0700

    Add Main.java

---
+++ VoicePassword/src/Main.java
@@ -0,0 +1,14 @@
+import java.awt.Dimension;
+import java.awt.Toolkit;
+
+import javax.swing.JFrame;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		
+		GUI.run();
+		
+	}
+
+}
