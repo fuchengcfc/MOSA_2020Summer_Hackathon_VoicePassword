ccca97e76c0963bb20299b1a9b36864250db8441
junojiahui <51941324+junojiahui@users.noreply.github.com>
2020-08-30 17:59:04 -0700

    Update GUI

--- VoicePassword/src/GUI.java
+++ VoicePassword/src/GUI.java
@@ -24,7 +24,7 @@ public class GUI extends JFrame {
 	/**
 	 * Launch the application.
 	 */
-	public static void main(String[] args) {
+	public static void run() {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
